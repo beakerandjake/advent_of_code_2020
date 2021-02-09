f3087ce16768bccaa4f757151b5e4a58dd5b0028
beakerandjake <jhoff462@gmail.com>
2021-02-09 12:58:32 -0700

    first pass part 2

--- src/day01.rs
+++ src/day01.rs
@@ -1,18 +1,16 @@
 use std::collections::HashSet;
 
+const targetSum: i32 = 2020;
 
 pub fn part1(input: String) {
 
-    let entries: HashSet<i32> = input
-        .lines()
-        .map(|line| line.trim().parse().expect("expected numeric input"))
-        .collect();
+    let entries = parse_entries(&input);
 
     for entry in entries.iter() {
-        let needed = 2020 - entry;
+        let needed = targetSum - entry;
 
         if entries.contains(&needed) {
-            println!("{} + {} = {}", entry, needed, entry*needed);
+            println!("{} * {} = {}", entry, needed, entry*needed);
             return;
         }
     }
@@ -20,3 +18,28 @@ pub fn part1(input: String) {
     println!("Could not find entries that sum to 2020");
 }
 
+
+pub fn part2(input: String) {
+    let entries = parse_entries(&input);
+
+    for first in entries.iter() {
+        for second in entries.iter() {
+            let third = targetSum - first - second;
+
+            if entries.contains(&third) {
+                println!("{} * {} * {} = {}", first, second, third, first*second*third);
+                return;
+            }
+        }
+    }
+
+    println!("Could not find 3 entries that sum to 2020");
+}
+
+// parses each line of the input into a number and collects into a HashSet.  
+fn parse_entries(input: &str) -> HashSet<i32> {
+    input
+        .lines()
+        .map(|line| line.trim().parse().expect("expected numeric input"))
+        .collect()
+}
