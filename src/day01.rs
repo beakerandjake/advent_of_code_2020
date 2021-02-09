e4ac9640d44383ad15d62d0f70ba0288a62369fe
beakerandjake <jhoff462@gmail.com>
2021-02-09 12:21:07 -0700

    first pass implementation of part 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,4 +1,22 @@
+use std::collections::HashSet;
 
-pub fn part1(inp: String) {
-    println!("{}", inp.chars().rev().collect::<String>());
+
+pub fn part1(input: String) {
+
+    let entries: HashSet<i32> = input
+        .lines()
+        .map(|line| line.trim().parse().expect("expected numeric input"))
+        .collect();
+
+    for entry in entries.iter() {
+        let needed = 2020 - entry;
+
+        if entries.contains(&needed) {
+            println!("{} + {} = {}", entry, needed, entry*needed);
+            return;
+        }
+    }
+
+    println!("Could not find entries that sum to 2020");
 }
+
