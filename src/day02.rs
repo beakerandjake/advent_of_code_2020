37dd0fb946ede4c1ce004d3c70aac4c6280a70b3
beakerandjake <jhoff462@gmail.com>
2021-02-11 14:20:17 -0700

    first pass rule generation

--- src/day02.rs
+++ src/day02.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 
+// rule struct contains all the information about a password rule.
 #[derive(Debug)]
 struct Rule {
     min: i32,
@@ -8,49 +9,66 @@ struct Rule {
 }
 
 impl Rule {
-    pub fn new(rule_str: &str) -> Result<Self, Box<dyn Error>> {
-        println!("parsing: {}", rule_str);
+    // rule string expected format is "X-Y Z" where x is min, y is max and z is the letter.
+    pub fn new(rule_str: &str) -> Result<Rule, Box<dyn Error>> {
+        // split on whitespace so we get ['X-Y', 'Z']
         let mut parts = rule_str.split_whitespace();
 
-        let min_max = parts
-            .next()
-            .ok_or("Could not parse min/max from rule string")?
-            .split('-');
+        // expect first part of rule string to be the min/max string 'X-Y'
+        let min_max_string = parts.next().ok_or("missing min/max portion of rule")?;
 
-        let letter = parts
-            .next()
-            .ok_or("Could not parse letter from rule string")?;
+        // parse the min/max numbers from the string.
+        let (min,max) = Rule::parse_min_max(&min_max_string)?;
 
-        println!("min/max:{} letter:{}", min_max, letter);
+        // expect second part of rule string to b e the letter portion.
+        let letter_string = parts.next().ok_or("missing letter portion of rule")?;
 
-        Ok(Rule {
-            min: 1,
-            max: 2,
-            letter: 'a',
-        })
+        // grab only the first character.
+        let letter = letter_string.chars().next().ok_or("letter not specified")?;
+      
+        Ok(Rule { min, max, letter: letter })
     }
 
-    fn parse_min_max(input:&str) -> Result<(i32,i32), Box<dyn Error>> {
-        // 13-14
-        let parts: Vec<i32> = input
-            .split('-')
-            .map(|x| x.parse::<i32>());
+    // parses a min/max string in the format 'X-Y' and returns the parsed min/max values.
+    fn parse_min_max(input: &str) -> Result<(i32,i32), Box<dyn Error>> {
+        let mut parts = input.split('-');
 
-        unimplemented!();
+        // expect min as first number.
+        let min: i32 = parts
+            .next()
+            .ok_or("min is missing")?
+            .parse()
+            .or(Err("min was not numeric"))?;
+
+        // expect max as second number
+        let max: i32 = parts
+            .next()
+            .ok_or("max is missing")?
+            .parse()
+            .or(Err("max was not numeric"))?;
+
+        Ok((min,max))
     }
 }
 
 pub fn part1(input: &str) -> Result<String, Box<dyn Error>> {
-    let line = input.lines().next().unwrap();
 
-    let result = parse_rule(line)?;
+    let mut valid_password_count = 0;
 
-    println!("rule: {:?} password: {}", result.0, result.1);
+    for line in input.lines() {
+        let (rule, password) = parse_rule_and_password(&line)?;
 
-    Ok(String::from("unimplemented"))
+        if validate_password(&password, &rule) {
+            valid_password_count += 1;
+        }
+    }
+
+    Ok(format!("Valid Passwords: {}", valid_password_count))
 }
 
-fn parse_rule(input: &str) -> Result<(Rule, &str), Box<dyn Error>> {
+// parse an input string in the format 'X-Y Z: P'
+// were 'X-Y Z' is the rule string and 'P' is the password. 
+fn parse_rule_and_password(input: &str) -> Result<(Rule, &str), Box<dyn Error>> {
     let mut parts = input.split(':');
 
     let rule_str = parts
@@ -61,5 +79,9 @@ fn parse_rule(input: &str) -> Result<(Rule, &str), Box<dyn Error>> {
         .next()
         .ok_or("Could not get password portion of input string")?;
 
-    Ok((Rule::new(rule_str)?, password)) 
+    Ok((Rule::new(&rule_str)?, password)) 
+}
+
+fn validate_password(password: &str, rule: &Rule) -> bool {
+    false
 }
