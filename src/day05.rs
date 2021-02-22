86720567fbb9e7990890d641b502b7fc9f6f6327
beakerandjake <jhoff462@gmail.com>
2021-02-22 15:53:08 -0700

    scaffold day 5

---
+++ src/day05.rs
@@ -0,0 +1,9 @@
+use std::error::Error;
+
+pub fn part1(input: &str) -> Result<String, Box<dyn Error>> {
+    unimplemented!();
+}
+
+pub fn part2(input: &str) -> Result<String, Box<dyn Error>> {
+    unimplemented!();
+}
