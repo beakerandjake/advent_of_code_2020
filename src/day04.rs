bbfef985f7b34d8a01af9bd1b4e647842a62f542
beakerandjake <jhoff462@gmail.com>
2021-02-22 15:16:17 -0700

    part 2

--- src/day04.rs
+++ src/day04.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::error::Error;
@@ -25,16 +26,36 @@ pub fn part1(input: &str) -> Result<String, Box<dyn Error>> {
     Ok(valid_passport_count.to_string())
 }
 
-pub type KeyValidatorFn = fn(&str) -> bool;
-
 pub fn part2(input: &str) -> Result<String, Box<dyn Error>> {
-    let mut key_validators: HashMap<&str, KeyValidatorFn> = HashMap::new();
-    key_validators.insert("byr", |x| validate_numeric_range(x, 1920, 2002));
-    key_validators.insert("iyr", |x| validate_numeric_range(x, 2010, 2020));
-    key_validators.insert("eyr", |x| validate_numeric_range(x, 2020, 2030));
-    key_validators.insert("ecl", |x| validate_numeric_range(x, 2020, 2030));
+    let hcl_regex = Regex::new(r"^#[\da-f]{6}$").unwrap();
+    let pid_regex = Regex::new(r"^[\d]{9}$").unwrap();
+    let hgt_regex = Regex::new(r"^(\d{1,3})(cm|in)$").unwrap();
+    let valid_eye_colors: HashSet<&str> = vec!["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+        .into_iter()
+        .collect();
 
-    Ok("asdf".to_string())
+    // map a passport field to a validation function that knows how to validate that fields value.
+    let mut validators: HashMap<&str, Box<dyn Fn(&str) -> bool>> = HashMap::new();
+    validators.insert("byr", Box::new(|x| validate_numeric_range(x, 1920, 2002)));
+    validators.insert("iyr", Box::new(|x| validate_numeric_range(x, 2010, 2020)));
+    validators.insert("eyr", Box::new(|x| validate_numeric_range(x, 2020, 2030)));
+    validators.insert("hcl", Box::new(|x| hcl_regex.is_match(x)));
+    validators.insert("ecl", Box::new(|x| valid_eye_colors.contains(x)));
+    validators.insert("pid", Box::new(|x| pid_regex.is_match(x)));
+    validators.insert("hgt", Box::new(|x| validate_height(x, &hgt_regex)));
+
+    let valid_passport_count = parse_passports(input)
+        .filter(|passport| {
+            validators
+                .iter()
+                .all(|(key, validator_fn)| match passport.get(key) {
+                    None => false,
+                    Some(value) => validator_fn(value),
+                })
+        })
+        .count();
+
+    Ok(valid_passport_count.to_string())
 }
 
 // returns an iterator which iterates each passport entry of the input
@@ -54,9 +75,19 @@ fn parse_passports<'a>(input: &'a str) -> Box<dyn Iterator<Item = HashMap<&'a st
 }
 
 fn validate_numeric_range(value: &str, min: i32, max: i32) -> bool {
-    if let Ok(parsed) = value.parse::<i32>() {
-        return parsed >= min && parsed <= max;
+    match value.parse::<i32>() {
+        Ok(parsed) => parsed >= min && parsed <= max,
+        Err(_e) => false,
     }
+}
 
-    false
+fn validate_height(value: &str, regex: &Regex) -> bool {
+    match regex.captures(value) {
+        None => false,
+        Some(ref capture) => match &capture[2] {
+            "cm" => validate_numeric_range(&capture[1], 150, 193),
+            "in" => validate_numeric_range(&capture[1], 59, 76),
+            _ => false,
+        },
+    }
 }
