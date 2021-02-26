46eff318e3320b8bbb17e8ee90e52d808ab01a38
beakerandjake <jhoff462@gmail.com>
2021-02-26 14:27:17 -0700

    part 1

--- src/day06.rs
+++ src/day06.rs
@@ -1,9 +1,27 @@
 use std::error::Error;
+use std::collections::HashSet;
 
 pub fn part1(input: &str) -> Result<String, Box<dyn Error>> {
-    unimplemented!();
+    
+    let mut total_count = 0;
+
+    for group in input.split("\r\n\r\n") {
+        let mut unique_answers = HashSet::new();
+
+        for person in group.lines() {
+            for answer in person.chars() {
+                unique_answers.insert(answer);
+            }
+        }
+
+        total_count += unique_answers.len();
+    }
+
+    Ok(total_count.to_string())
 }
 
 pub fn part2(input: &str) -> Result<String, Box<dyn Error>> {
     unimplemented!();
 }
+
+
