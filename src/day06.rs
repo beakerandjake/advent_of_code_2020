135c87e6e8208fd7644b46a79f020e4f7914593c
beakerandjake <jhoff462@gmail.com>
2021-02-26 14:38:48 -0700

    part 2

--- src/day06.rs
+++ src/day06.rs
@@ -1,8 +1,8 @@
-use std::error::Error;
+use std::collections::HashMap;
 use std::collections::HashSet;
+use std::error::Error;
 
 pub fn part1(input: &str) -> Result<String, Box<dyn Error>> {
-    
     let mut total_count = 0;
 
     for group in input.split("\r\n\r\n") {
@@ -21,7 +21,21 @@ pub fn part1(input: &str) -> Result<String, Box<dyn Error>> {
 }
 
 pub fn part2(input: &str) -> Result<String, Box<dyn Error>> {
-    unimplemented!();
+    let mut total_count = 0;
+
+    for group in input.split("\r\n\r\n") {
+        let mut answer_counts = HashMap::new();
+        
+        for person in group.lines() {
+            for answer in person.chars() {
+                *answer_counts.entry(answer).or_insert(0) += 1;
+            }
+        }
+        
+        // only increment by number of questions that were answered by the whole group.
+        let group_count = group.lines().count();
+        total_count += answer_counts.values().filter(|&&x| x == group_count).count();
+    }
+
+    Ok(total_count.to_string())
 }
-
-
