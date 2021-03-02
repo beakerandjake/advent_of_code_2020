a94c50197d593a93d87f60f6ccec5a9576722eed
beakerandjake <jhoff462@gmail.com>
2021-03-02 12:10:07 -0700

    minor speed up

--- src/day07.rs
+++ src/day07.rs
@@ -37,54 +37,76 @@ pub fn part1(input: &str) -> Result<String, Box<dyn Error>> {
     let mut bag_rules = HashMap::new();
 
     for line in input.lines() {
-        let (bag, child_bags) = parse_input_line(line)?;
+        let (bag, child_bags) = parse_part1_input_line(line)?;
         bag_rules.insert(bag, child_bags);
     }
-    
+
     let mut target_count = 0;
     let target_color = "shiny gold";
+    let mut known_holders = HashSet::new();
 
     for &bag_color in bag_rules.keys() {
-        if bag_color != target_color && search_child_bags(target_color, bag_color, &bag_rules) {
+        if bag_color != target_color
+            && search_part1(bag_color, target_color, &bag_rules, &mut known_holders)
+        {
             target_count = target_count + 1;
+            known_holders.insert(bag_color);
         }
     }
 
     Ok(target_count.to_string())
 }
 
-pub fn part2(_input: &str) -> Result<String, Box<dyn Error>> {
-    unimplemented!();
-}
-
-fn parse_input_line<'a>(input: &'a str) -> Result<(&'a str, Vec<Requirement>), Box<dyn Error>> {
+fn parse_part1_input_line<'a>(input: &'a str) -> Result<(&'a str, Vec<&str>), Box<dyn Error>> {
     let mut parts = input.split(" bags contain ");
     let bag_name = parts.next().ok_or("could not get bag name.")?;
-    let child_bags: Vec<Requirement> = parts
+
+    lazy_static! {
+        static ref PARSE_CHILD_REGEX: Regex = Regex::new(r"\d+ (.*) bags?\.?$").unwrap();
+    }
+
+    let child_bags: Vec<&str> = parts
         .next()
         .ok_or("could not get child bags")?
         .split(", ")
-        .filter_map(|x| Requirement::parse(x))
+        .filter_map(|x| match PARSE_CHILD_REGEX.captures(x) {
+            None => None,
+            Some(captures) => Some(captures.get(1).unwrap().as_str()),
+        })
         .collect();
 
     Ok((bag_name, child_bags))
 }
 
-fn search_child_bags(target_color: &str, current_color: &str, bag_rules: &HashMap<&str, Vec<Requirement>>) -> bool {
-    if target_color == current_color {
+fn search_part1<'a>(
+    current_bag_color: &'a str,
+    target_bag_color: &str,
+    all_bags: &HashMap<&str, Vec<&'a str>>,
+    known_holders: &mut HashSet<&'a str>,
+) -> bool {
+    if current_bag_color == target_bag_color {
         return true;
     }
-    
-    let child_bags = match bag_rules.get(current_color) {
+
+    if known_holders.contains(current_bag_color) {
+        return true;
+    }
+
+    let child_bags = match all_bags.get(current_bag_color) {
         None => return false,
-        Some(x) => x
+        Some(x) => x,
     };
-    
-    for child_bag in child_bags {
-        if search_child_bags(target_color, child_bag.color, bag_rules) {
+
+    for &current_color in child_bags {
+        if search_part1(current_color, target_bag_color, all_bags, known_holders) {
+            known_holders.insert(current_color);
             return true;
         }
     }
 
     return false;
+}
+
+pub fn part2(_input: &str) -> Result<String, Box<dyn Error>> {
+    unimplemented!();
 }
