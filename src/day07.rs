b12e8bca52f54395d09d4fadb92a4bdb7dd86bae
beakerandjake <jhoff462@gmail.com>
2021-03-03 13:05:57 -0700

    first pass speed up

--- src/day07.rs
+++ src/day07.rs
@@ -1,110 +1,118 @@
-use lazy_static::lazy_static;
-use regex::Regex;
 use std::collections::HashMap;
-use std::collections::HashSet;
 use std::error::Error;
 
-#[derive(Debug)]
-struct Requirement<'a> {
-    quantity: u32,
-    color: &'a str,
+type BagName<'a> = (&'a str, &'a str);
+type ChildBag<'a> = (u32, BagName<'a>);
+type BagMap<'a> = HashMap<BagName<'a>, Bag<'a>>;
+
+#[derive(Eq, Hash)]
+struct Bag<'a> {
+    words: Vec<&'a str>,
 }
 
-impl<'a> Requirement<'a> {
-    fn parse(input: &'a str) -> Option<Requirement> {
-        lazy_static! {
-            static ref PARSE_INPUT_REGEX: Regex = Regex::new(r"(\d+) (.*) bags?\.?$").unwrap();
+impl<'a> Bag<'a> {
+    fn new(input: &'a str) -> Bag {
+        Bag {
+            words: input.split_whitespace().collect(),
         }
+    }
 
-        // handle special case of no requirement.
-        if input == "no other bags." {
+    fn name(&self) -> BagName<'a> {
+        (self.words[0], self.words[1])
+    }
+
+    fn children(&self) -> BagIter {
+        BagIter {
+            bag: self,
+            child_index: 0,
+        }
+    }
+}
+
+impl<'a> PartialEq for Bag<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.words[0] == other.words[0] && self.words[1] == other.words[1]
+    }
+}
+
+pub struct BagIter<'a> {
+    bag: &'a Bag<'a>,
+    child_index: usize,
+}
+
+impl<'a> Iterator for BagIter<'a> {
+    type Item = ChildBag<'a>;
+    fn next(&mut self) -> Option<ChildBag<'a>> {
+        let index = self.child_index * 4 + 4;
+
+        if index >= self.bag.words.len() {
             return None;
         }
 
-        let (quantity, color) = match PARSE_INPUT_REGEX.captures(input) {
-            Some(capture) => match (&capture[1]).parse::<u32>() {
-                Ok(num) => (num, capture.get(2).unwrap().as_str()),
-                Err(_e) => return None,
-            },
-            None => return None,
-        };
+        let count = self.bag.words[index];
 
-        Some(Requirement { quantity, color })
+        if count == "no" {
+            return None;
+        }
+
+        let parsed_count: u32 = count.parse().unwrap();
+        let name = (self.bag.words[index + 1], self.bag.words[index + 2]);
+
+        self.child_index += 1;
+
+        Some((parsed_count, name))
     }
 }
 
 pub fn part1(input: &str) -> Result<String, Box<dyn Error>> {
-    let mut bag_rules = HashMap::new();
-
-    for line in input.lines() {
-        let (bag, child_bags) = parse_part1_input_line(line)?;
-        bag_rules.insert(bag, child_bags);
-    }
-
-    let mut target_count = 0;
-    let target_color = "shiny gold";
-    let mut known_holders = HashSet::new();
-
-    for &bag_color in bag_rules.keys() {
-        if bag_color != target_color
-            && search_part1(bag_color, target_color, &bag_rules, &mut known_holders)
-        {
-            target_count = target_count + 1;
-            known_holders.insert(bag_color);
-        }
-    }
-
-    Ok(target_count.to_string())
-}
-
-fn parse_part1_input_line<'a>(input: &'a str) -> Result<(&'a str, Vec<&str>), Box<dyn Error>> {
-    let mut parts = input.split(" bags contain ");
-    let bag_name = parts.next().ok_or("could not get bag name.")?;
-
-    lazy_static! {
-        static ref PARSE_CHILD_REGEX: Regex = Regex::new(r"\d+ (.*) bags?\.?$").unwrap();
-    }
-
-    let child_bags: Vec<&str> = parts
-        .next()
-        .ok_or("could not get child bags")?
-        .split(", ")
-        .filter_map(|x| match PARSE_CHILD_REGEX.captures(x) {
-            None => None,
-            Some(captures) => Some(captures.get(1).unwrap().as_str()),
+    let bags: BagMap = input
+        .lines()
+        .map(|line| {
+            let bag = Bag::new(line);
+            (bag.name(), bag)
         })
         .collect();
 
-    Ok((bag_name, child_bags))
-}
+    let target_bag = ("shiny", "gold");
+    let mut found_count = 0;
+    let mut visited: HashMap<BagName, bool> = HashMap::new();
 
-fn search_part1<'a>(
-    current_bag_color: &'a str,
-    target_bag_color: &str,
-    all_bags: &HashMap<&str, Vec<&'a str>>,
-    known_holders: &mut HashSet<&'a str>,
-) -> bool {
-    if current_bag_color == target_bag_color {
-        return true;
-    }
+    for bag in &bags {
+        if *bag.0 == target_bag {
+            continue;
+        }
 
-    if known_holders.contains(current_bag_color) {
-        return true;
-    }
-
-    let child_bags = match all_bags.get(current_bag_color) {
-        None => return false,
-        Some(x) => x,
-    };
-
-    for &current_color in child_bags {
-        if search_part1(current_color, target_bag_color, all_bags, known_holders) {
-            known_holders.insert(current_color);
-            return true;
+        if search_part1(bag.1, target_bag, &bags, &mut visited) {
+            visited.insert(*bag.0, true);
+            found_count += 1;
         }
     }
 
-    return false;
+    Ok(found_count.to_string())
+}
+
+fn search_part1<'a>(
+    current_bag: &'a Bag<'a>,
+    target_bag_name: BagName<'a>,
+    all_bags: &'a BagMap<'a>,
+    visited: &mut HashMap<BagName<'a>, bool>,
+) -> bool {
+    let current_bag_name = current_bag.name();
+
+    if let Some(&v) = visited.get(&current_bag_name) {
+        return v;
+    }
+
+    if current_bag_name == target_bag_name {
+        return true;
+    }
+
+    current_bag.children().any(|x| {
+        let child_bag = all_bags.get(&x.1).unwrap();
+        let found = search_part1(child_bag, target_bag_name, &all_bags, visited);
+        visited.insert(x.1, found);
+        found
+    })
 }
 
 pub fn part2(_input: &str) -> Result<String, Box<dyn Error>> {
